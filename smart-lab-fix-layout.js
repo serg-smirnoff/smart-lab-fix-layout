9abe51baf5632a4ec5cd18456a9b3c6e9e90f27d
root <info@serg-smirnoff.com>
2020-05-06 01:59:02 +0300

    Добавлена возможность скрыть индикаторы

--- smart-lab-fix-layout.js
+++ smart-lab-fix-layout.js
@@ -9,12 +9,23 @@
 // @description 05.05.2020, 00:24:10
 // ==/UserScript==
 
+
+
 $(document).ready(function(){
+  $('table.trades').before('<div class="hide_info">Скрыть индикаторы</div>');
+  $('.hide_info').css({'background':'#F59300','cursor':'pointer','color':'#fff','height':'30px','line-height':'30px','width':'200px','text-align':'center','margin':'10px auto','border-radius':'5px'});
+  $('.hide_info').click(function(){
+    if ($('.hide_info').text() == 'Скрыть индикаторы')
+      $('.hide_info').text('Показать индикаторы');
+    else 
+      $('.hide_info').text('Скрыть индикаторы');
+    
+    $('table.trades').toggle('slow');
+  });
   $('.sidebar_left, .sidebar_right, #inindex, .content_desc, #mood, .console').hide();
   $('#content').css("width","100%");
   $('#content_box').css({"width":"70%", "margin-left": "auto", "margin-right": "auto", "float": "unset"});
   $('#header .h1 a').css({"font-size": "29px", "line-height": "32px"});
   $('.topic .content img').css("width","98%");
-  $('.fastlinks ol li:before').css("content","unset");
   $('.topic .voting').css('display','none');
 })
